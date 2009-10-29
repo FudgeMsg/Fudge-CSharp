75b47d44ce155af9e02d45b4ce91b2db029154a2
t0rx <t0rxon@googlemail.com>
2009-10-29 21:41:25 +0000

    [FRN-7] Updated the assembly meta-info.

--- FudgeTests/Properties/AssemblyInfo.cs
+++ FudgeTests/Properties/AssemblyInfo.cs
@@ -1,4 +1,19 @@
-﻿using System.Reflection;
+﻿/**
+ * Copyright (C) 2009 - 2009 by OpenGamma Inc. and other contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +23,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FudgeTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FudgeTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyCompany("OpenGamma Inc.")]
+[assembly: AssemblyProduct("FudgeMsg tests")]
+[assembly: AssemblyCopyright("Copyright © 2009 OpenGamma Inc. and other contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,8 +44,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
